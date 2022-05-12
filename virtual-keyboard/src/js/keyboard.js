7c88228384d51769f48102c36cacb13a657cf45d
manbalboy <manbalboy@hanmail.net>
2022-05-12 22:46:43 +0900

    keyboard key event 적용

--- virtual-keyboard/src/js/keyboard.js
+++ virtual-keyboard/src/js/keyboard.js
@@ -1,6 +1,10 @@
 export class Keyboard {
   #swichEl;
   #fontSelectEl;
+  #containerEl;
+  #keyboardEl;
+  #inputGroupEl;
+  #inputEl;
 
   constructor() {
     this.#assignElement();
@@ -8,21 +12,56 @@ export class Keyboard {
   }
 
   #assignElement() {
-    this.#swichEl = document.getElementById("switch");
-    this.#fontSelectEl = document.getElementById("font");
+    this.#containerEl = document.getElementById("container");
+    this.#swichEl = this.#containerEl.querySelector("#switch");
+    this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
+    this.#inputGroupEl = this.#containerEl.querySelector("#input-group");
+    this.#inputEl = this.#inputGroupEl.querySelector("#input");
   }
 
   #addEvent() {
-    this.#swichEl.addEventListener("change", (evnet) => {
-      document.documentElement.setAttribute(
-        "theme",
-        evnet.target.checked ? "dark-mode" : ""
-      );
-      console.log(evnet.target.checked);
-    });
+    this.#swichEl.addEventListener("change", this.#onChangeTheme);
 
-    this.#fontSelectEl.addEventListener("change", (event) => {
-      document.body.style.fontFamily = event.target.value;
-    });
+    this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+
+    document.addEventListener("keydown", this.#onKeyDown.bind(this));
+
+    document.addEventListener("keyup", this.#onKeyUp.bind(this));
+
+    this.#inputEl.addEventListener("input", this.#onInput);
+  }
+
+  #onKeyUp(event) {
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.remove("active");
+  }
+
+  #onKeyDown(event) {
+    this.#inputGroupEl.classList.toggle(
+      "error",
+      /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key)
+    );
+
+    this.#keyboardEl
+      .querySelector(`[data-code=${event.code}]`)
+      ?.classList.add("active");
+  }
+
+  #onInput(event) {
+    event.target.value = event.target.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
+  }
+
+  #onChangeTheme(event) {
+    document.documentElement.setAttribute(
+      "theme",
+      event.target.checked ? "dark-mode" : ""
+    );
+    console.log(event.target.checked);
+  }
+
+  #onChangeFont(event) {
+    document.body.style.fontFamily = event.target.value;
   }
 }
